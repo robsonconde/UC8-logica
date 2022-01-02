5c48f89dbc3b2d397de474caf271e964af42fa50
robsonconde <robtcmoraes@gmail.com>
2022-01-02 15:06:55 -0300

    Atividade cadastro de eventos

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var data = new Date(2022, 3, 12 );
 var dataAtual = new Date();
 var totalParticipantes = 55;
 
-//informa nome
+// início informa nome
 console.log("Informe o nome:");
-//fim informa nome
+// fim informa nome
 
-// teste idade
+// início teste idade
 console.log("Informe a idade:");
 if (idade >= 18) {
     console.log("Selecione o evento desejado");
 } else {
     console.log("inválido, inscrição somente para maiores de idade");
-    //fim do algoritmo
+    // fim do algoritmo
 }
 // fim teste idade
 
-//teste data
+// início teste data
 if (data <= dataAtual) {
     console.log("Data inválida. Por favor selecione outra opção");
 } else if(totalParticipantes >= 100) {
